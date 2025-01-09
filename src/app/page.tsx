63f81d2b388ac1451ef128be3881128734676774
tkl0000 <thomasli2025@gmail.com>
2025-01-08 19:41:15 -0500

    lmfao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,45 @@
+"use client";
 import Image from 'next/image';
+import {useState} from 'react';
 
 export default function Home() {
+
+  const [imageHover, setImageHover] = useState(false);
+
   return (
     <div className="flex flex-col items-center p-3 gap-3">
-      <Image
-        src="/images/dog.png"
-        width={500}
-        height={500}
-        alt="dog" />
-      <p>
-        &quot;Hey&quot; - Sun Tzu
-      </p>
+      <div
+        className="relative h-60 w-80"
+        onMouseEnter={() => setImageHover(true)}
+        onMouseLeave={() => setImageHover(false)}
+      >
+        {imageHover ? (
+          <Image
+            src="/images/dog.png"
+            layout="fill"
+            objectFit="contain"
+            alt="dog"
+          />
+        ) : (
+          <Image
+            src="/images/me.png"
+            layout="fill"
+            objectFit="contain"
+            alt="me"
+          />
+        )}
+      </div>
+      <div>
+        {imageHover ? (
+          <p>
+          &quot;Hey&quot; - Sun Tzu
+          </p>
+        ) : (
+          <p>
+          &quot;Welcome to my website&quot; - Thomas Li
+        </p>
+        )}
+      </div>
     </div>
   );
 }
