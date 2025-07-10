f63e95ac0afedaaa45f8f010fe9ff7a6fd12be77
tkl0000 <thomasli2025@gmail.com>
2025-07-10 11:37:19 -0400

    Better on Mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from 'next/image';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 // import { MathJaxContext, MathJax } from 'better-react-mathjax';
 // import Link from 'next/link';
@@ -39,62 +39,136 @@ const descriptions: { [key: string]: React.ReactNode } = {
   // </div>
 // ) */}
 
-const Title = (imageHover: string) => {
+const Title = (imageHover: string, imageFocus: string, active: boolean) => {
   return (
     <div className="font-times w-fit mt-20">
       {/* thomas 康毅 li */}
       <div className="flex text-8xl lg:text-9xl hover:underline cursor-pointer"> 
         <a href="mailto:thomaskl@cmu.edu">thomas li</a>
       </div>
-      <AnimatePresence mode="wait">
-        (<motion.div
-          key={imageHover}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.2 }}
-          className="font-times flex justify-end">
-          {imageHover === "" ? defaultText : (descriptions[imageHover])}
-        </motion.div> )
-      </AnimatePresence>
+      <div className="lg:hidden">
+        <AnimatePresence mode="wait">
+          (<motion.div
+            key={`${imageFocus}-${active}`}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="font-times flex justify-end">
+            {active === false ? defaultText : (descriptions[imageFocus])}
+          </motion.div> )
+        </AnimatePresence>
+      </div>
+      <div className="hidden lg:block">
+        <AnimatePresence mode="wait">
+          (<motion.div
+            key={imageHover}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="font-times flex justify-end">
+            {imageHover === "" ? defaultText : (descriptions[imageHover])}
+          </motion.div> )
+        </AnimatePresence>
+      </div>
     </div>
   )
 }
 
 const defaultText = "CS @ Carnegie Mellon University"
 
-const Carousel = (setImageHover: Dispatch<SetStateAction<string>>) => {
+const Carousel = (setImageHover: Dispatch<SetStateAction<string>>, setImageFocus: Dispatch<SetStateAction<string>>, setActive: Dispatch<SetStateAction<boolean>>) => {
+  const itemsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const focusedImage = String(entry.target.getAttribute('alt'));
+            setImageFocus(focusedImage)
+          }
+        });
+      },
+      { root: null, threshold: 0.7 } // 50% visible = considered "focused"
+    );
+
+    itemsRef.current.forEach(item => {
+      if (item) observer.observe(item);
+    });
+
+    return () => {
+      itemsRef.current.forEach(item => {
+        if (item) observer.unobserve(item);
+      });
+    };
+  }, [])
+
   return (
-    Object.keys(descriptions).map((key) => (
-      <Image
-        key={key}
-        src={`/images/${key}.png`}
-        width={300}
-        height={100}
-        alt={key}
-        onMouseEnter={() => { setImageHover(key) }}
-        onMouseLeave={() => { setImageHover("") }}
-      />
-    ))
-  )
+      Object.keys(descriptions).map((key, index) => (
+          <Image
+            src={`/images/${key}.png`}
+            width={300}
+            height={100}
+            alt={key}
+            key={key}
+            data-index={index}
+            ref={(el) => {
+              itemsRef.current[index] = el;
+            }}
+            onMouseEnter={() => {
+              setImageHover(key)
+            }}
+            onMouseLeave={() => {
+              setImageHover("")
+            }}
+          />
+      ))
+  );
 }
 
 const Home = () => {
 
-  const [imageHover, setImageHover] = useState("");
+  const [imageHover, setImageHover] = useState(""); // desktop
+  const [imageFocus, setImageFocus] = useState("Default Focus"); // mobile
+  const [active, setActive] = useState(false) // mobile
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setActive(true);  
+    };
+
+    const container = carouselRef.current;
+    if (container) {
+      container.addEventListener("scroll", handleScroll, { passive: true });
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
 
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="flex flex-col lg:flex-row h-screen justify-between">
+      className="flex flex-col lg:flex-row h-screen lg:gap-0 lg:justify-between">
       <div className="flex flex-col items-center lg:items-start">
-        {Title(imageHover)}
+        {Title(imageHover, imageFocus, active)}
       </div>
-      <div className="flex flex-row gap-5 overflow-x-auto mb-10 lg:flex-col lg:overflow-y-auto lg:max-h-screen lg:mr-10 lg:mb-0">
-        {Carousel(setImageHover)}
-      </div> 
+      <div className="flex flex-col justify-center h-screen lg:flex-row lg:justify-end lg:flex-grow ">
+        {/* {imageHover} */}
+        {/* {imageFocus} */}
+        <div className="flex flex-row gap-5 overflow-x-auto lg:flex-col lg:overflow-y-auto lg:max-h-screen lg:mr-10 lg:mb-0 lg:w-full lg:items-end"
+              ref={carouselRef}>
+          {Carousel(setImageHover, setImageFocus, setActive)}
+        </div> 
+      </div>
     </motion.div>
   );
 }
