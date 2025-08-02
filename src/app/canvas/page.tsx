d42c75e437f22c9b540fccfd91ab4f394f77cef0
tkl0000 <thomasli2025@gmail.com>
2025-08-02 16:07:00 -0400

    canvas

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -1,8 +1,36 @@
+"use client";
+import { useEffect, useRef } from "react"
+
 const Canvas = () => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null)
+    useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const resizeCanvas = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    };
+
+    resizeCanvas();
+    window.addEventListener('resize', resizeCanvas);
+
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      ctx.fillStyle = 'white'
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    return () => {
+        window.removeEventListener('resize', resizeCanvas);
+        };
+    }, []);
+
     return (
-        <div>
-            Canvas
-        </div>
+        <canvas
+            ref={canvasRef}
+            style={{ display: 'block', position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh' }}
+        />
     )
 }
 
