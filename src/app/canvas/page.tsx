2480cfa61a7d90a12f662ff396aeb909f587f45c
tkl0000 <thomasli2025@gmail.com>
2025-08-03 22:25:24 -0400

    idfk

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -4,28 +4,34 @@ import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react"
 interface SliderProps {
     title: string,
     value: number,
-    setValue: Dispatch<SetStateAction<number>>
+    setValue: Dispatch<SetStateAction<number>>,
+    lower: number,
+    upper: number,
 }
 
-const Slider: React.FC<SliderProps> = ({ title, value, setValue }) => {
+const Slider: React.FC<SliderProps> = ({ title, value, setValue, lower, upper }) => {
     return (
-        <div className="flex flex-row gap-5 p-3 rounded-lg bg-white text-black w-fit fixed right-5 bottom-5 z-10">
-            <p className="">
-                {/* {title}: {value} */}
-                {value}
-            </p>
-            <input
-                type="range"
-                min="0"
-                max="200"
-                value={value}
-                onChange={(e) => {
-                    const newValue = e.target.value as unknown as number
-                    setValue(e.target.value as unknown as number)
-                    console.log(newValue)
-                }}
-                className="w-20">
-            </input>
+        <div className="flex flex-col gap-2 p-3 rounded-lg bg-white text-black w-full">
+            <div className="flex flex-row justify-end">
+                {title}
+            </div>
+            <div className="flex flex-row">
+                <div className="mr-4">
+                    {value}
+                </div>
+                <input
+                    type="range"
+                    min={lower}
+                    max={upper}
+                    value={value}
+                    onChange={(e) => {
+                        const newValue = e.target.value as unknown as number
+                        setValue(e.target.value as unknown as number)
+                        console.log(newValue)
+                    }}
+                    className="w-20">
+                </input>
+            </div>
         </div>
     );
 }
@@ -35,10 +41,12 @@ const Canvas = () => {
     const [pixelSize, setPixelSize] = useState<number>(10);
     const [burstSize, setBurstSize] = useState<number>(100);
     const burstSizeRef = useRef(burstSize)
+    const pixelSizeRef = useRef(pixelSize)
 
     useEffect(() => {
         burstSizeRef.current = burstSize;
-    }, [burstSize]);
+        pixelSizeRef.current = pixelSize
+    }, [burstSize, pixelSize]);
 
     const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
     const setFirework = async (x: number, y: number) => {
@@ -53,7 +61,7 @@ const Canvas = () => {
         //     await delay(25)
         // }
         console.log(burstSizeRef.current)
-        for (var rad = 0; rad < 2 * Math.PI; rad += Math.PI / 8) {
+        for (var rad = 0; rad < 2 * Math.PI; rad += Math.PI / 12) {
             dropPixel(x, endY, burstSizeRef.current * Math.cos(rad), burstSizeRef.current * Math.sin(rad))
         }
     }
@@ -64,7 +72,8 @@ const Canvas = () => {
         const ctx = canvas.getContext('2d')
         if (!ctx) return
         ctx.fillStyle = color;
-        ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+        const curPixelSize = pixelSizeRef.current
+        ctx.fillRect(x * curPixelSize, y * curPixelSize, curPixelSize, curPixelSize);
     };
 
     const dropPixel = async (x: number, y: number, velocityX: number, velocityY: number) => {
@@ -73,8 +82,9 @@ const Canvas = () => {
         const canvas = canvasRef.current
         if (!canvas) return
         const rect = canvas.getBoundingClientRect();
-        while (0 < curX && curX < rect.right / pixelSize && 0 < curY && curY < rect.bottom / pixelSize) {
-            drawPixel(curX, curY, 'white')
+        const curPixelSize = pixelSizeRef.current
+        while (0 < curX && curX < rect.right / curPixelSize && 0 < curY && curY < rect.bottom / curPixelSize) {
+            drawPixel(Math.floor(curX), Math.floor(curY), 'white')
             curX += velocityX / 100
             curY += velocityY / 100
             velocityY += 0.01 * 70 // gravitational constant XD
@@ -86,8 +96,9 @@ const Canvas = () => {
         const canvas = canvasRef.current
         if (!canvas) return
         const rect = canvas.getBoundingClientRect();
-        const x = Math.floor((e.clientX - rect.left) / pixelSize);
-        const y = Math.floor((e.clientY - rect.top) / pixelSize);
+        const curPixelSize = pixelSizeRef.current
+        const x = Math.floor((e.clientX - rect.left) / curPixelSize);
+        const y = Math.floor((e.clientY - rect.top) / curPixelSize);
         console.log(burstSize)
         setFirework(x, y)
         // dropPixel(x, y, 0, 0)
@@ -97,8 +108,9 @@ const Canvas = () => {
         const canvas = canvasRef.current
         if (!canvas) return
         const rect = canvas.getBoundingClientRect();
-        const x = Math.floor((e.clientX - rect.left) / pixelSize);
-        const y = Math.floor((e.clientY - rect.top) / pixelSize);
+        const curPixelSize = pixelSizeRef.current
+        const x = Math.floor((e.clientX - rect.left) / curPixelSize);
+        const y = Math.floor((e.clientY - rect.top) / curPixelSize);
         drawPixel(x, y, 'white');
     };
 
@@ -152,7 +164,10 @@ const Canvas = () => {
                 ref={canvasRef}
                 style={{ display: 'block', position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh' }}
             />
-            <Slider title="Burst Size" value={burstSize} setValue={setBurstSize} />
+            <div className="fixed bottom-5 right-5 z-10 flex flex-col gap-5">
+                <Slider title="Pixel Size" value={pixelSize} setValue={setPixelSize} lower={1} upper={40} />
+                <Slider title="Burst Size" value={burstSize} setValue={setBurstSize} lower={0} upper={200} />
+            </div>
         </div>
     )
 }
